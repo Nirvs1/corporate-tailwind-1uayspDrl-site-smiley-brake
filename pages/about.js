772e41445edb503daeb5f9922d3c0c042c86ec7a
Nirvan <93690524+Nirvs1@users.noreply.github.com>
2022-07-28 14:58:41 +0100

    First commit

---
+++ pages/about.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { getProps } from "@builtjs/site";
+import Error from "next/error";
+import { withRouter } from "next/router";
+import SEO from "@/templates/head/seo/seo";
+import Cover3 from "@/templates/covers/cover-3/cover-3";
+import Layout from "@/layout/layout";
+
+const Page = (props) => {
+  if (props.error) {
+    return <Error statusCode={props.error.code} />
+  }
+  return (
+     <>
+	<SEO content={props.aboutSeoContent} />
+		<Layout headerContent={props.headerContent}  footerContent={props.footerContent} >
+			 <Cover3 content={props.aboutLandingContent} />
+			</Layout>
+		</>
+  );
+};
+
+export default withRouter(Page);
+
+export async function getStaticProps() {
+  let props = await getProps({pageSlug: 'about'});
+  return {
+    props: props,
+    revalidate: 10
+  };
+}
---
+++ pages/services.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { getProps } from "@builtjs/site";
+import Error from "next/error";
+import { withRouter } from "next/router";
+import SEO from "@/templates/head/seo/seo";
+import Banner1 from "@/templates/banners/banner-1/banner-1";
+import Block2 from "@/templates/blocks/block-2/block-2";
+import Cover4 from "@/templates/covers/cover-4/cover-4";
+import Layout from "@/layout/layout";
+
+const Page = (props) => {
+  if (props.error) {
+    return <Error statusCode={props.error.code} />
+  }
+  return (
+     <>
+	<SEO content={props.servicesSeoContent} />
+		<Layout headerContent={props.headerContent}  footerContent={props.footerContent} >
+			 <Cover4 content={props.servicesLandingContent} />
+			 <Block2 content={props.servicesBlockContent} />
+			 <Banner1 content={props.contactTeaserContent} />
+			</Layout>
+		</>
+  );
+};
+
+export default withRouter(Page);
+
+export async function getStaticProps() {
+  let props = await getProps({pageSlug: 'service'});
+  return {
+    props: props,
+    revalidate: 10
+  };
+}
